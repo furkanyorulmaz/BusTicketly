97128b3a53bffad84c12934248954bfbc738ed02
kubra <kbr.flk@hotmail.com>
2020-12-17 19:49:39 +0300

    base files commited

---
+++ Backend/base/contactUs_RU.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5 maxmum-scale=1.0"/>
+    <link rel="stylesheet" type="text/css" href="../main.css">
+    <title>CONTACT US RU</title>
+</head>
+<body>
+<!-- Navbar -->
+<div class="navbar">
+
+    <!-- Left-aligned links (default) -->
+    <a href="homepage_RU.php">Homepage</a>
+    <a href="aboutUs_RU.php">About Us</a>
+    <a href="contactUs_RU.php">Contact Us</a>
+    <a href="support_RU.php">Support</a>
+
+    <!-- Right-aligned links -->
+    <div class="navbar-right">
+        <a href="../registered/registerUserProfile.php">My Profile</a>
+        <a href="../logout.php">Logout</a>
+    </div>
+</div>
+
+<div class="container">
+    <div style="text-align:center">
+        <h2>CONTACT WITH US</h2>
+        <p>Filled the form contact us with officer</p>
+    </div>
+    <div class="row">
+        <form action="#" method="POST">
+
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" placeholder="Enter email..." required>
+
+            <label for="subject">Subject</label>
+            <input type="text" id="subject" name="subject" placeholder="What you want mentione ?" required>
+
+            <label for="message">Message</label>
+            <textarea class="msg_content" name="message" placeholder="Write message..." style="width: 100%;
+    height: 165px;
+    padding: 15px;
+    margin: 5px 0 22px 0;
+    display: inline-block;
+    border: 3px solid #b7d7e8;
+    background: #f1f1f1;" required></textarea>
+
+            <button type="submit" class="addjourneybtn" name="send_contact">Send</a></button>
+        </form>
+    </div>
+</div>
+
+<footer class="main_footer">
+    <h5 id="footer_text"> All Rights Reserved By BUS TICKETLY. © 2020</h5>
+</footer>
+
+</body>
+</html>
+
+<?php
+session_start();
+#$conn = mysqli_connect("localhost", "root", "", "busdb");
+include("../dbconnect.php");
+if (isset($_POST['send_contact'])) {
+
+    $mailForm = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    #int random number generator
+    $feedbackId = rand(100, 999);
+    $feedbacks = "INSERT INTO comments(feedbackId,subject,message,email) VALUES ('$feedbackId','$subject','$message','$mailForm')";
+    if (isset($conn)) {
+        $result = mysqli_query($conn, $feedbacks);
+
+        #echo "" . $mailForm . "\n" . $subject . "\n" . $message . "\n";
+        if (!$result) {
+            #echo "Error, check your code !!!";
+            echo '<script> 
+            if(confirm("Message can not send to Officer.\n Do you want to continue?")) {
+                window.location.href = "../registered/registerUserProfile.php"
+            }</script>';
+            exit();
+        } else {
+            #echo "Message send to Officer";
+            echo '<script> 
+            if(confirm("Message send to Officer.\n Do you want to continue?")) {
+                window.location.href = "../registered/registerUserProfile.php"
+            }</script>';
+            exit();
+        }
+    }
+}
+?>
